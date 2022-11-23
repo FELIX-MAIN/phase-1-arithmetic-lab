20aea1a8d67e2c18de0e2c2ab3c1d4a42d35f824
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-23 12:06:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+// Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//random
+const randomNumber = mathrandom() *100;
+console.log(randomNumber);
+
+//mod
+const num3 = 24;
+const num4 = 5;
+let mod = num3 / num4;
+console.log(mod);
+
+//max
+console.log(Math.max(2, 4, 6, 8, 10, 12, 13, 14 ,16, 18, 20));
